fd3df7d41b41cf049544a1e44353e92a713e7a6f
hasanRafi2002 <rafi2002ten@gmail.com>
2025-12-06 02:09:57 +0600

    i am rich

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Md Tawhid Hasan Rafi on 1/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
